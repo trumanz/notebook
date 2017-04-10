03aa8e90be631ff7df644b8d7ab49e99959a41b6
Truman Zhou <Truman.Zhou@emc.com>
2017-04-11 05:20:57 +0800

    fix href bug

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@ function covert_collections_to_json(collections_str)
 {
 	
 	var collections = collections_str.split("lable: ");
-	console.log(collections);
+	//console.log(collections);
 	var blog_collection = null;
 	for(i in collections){
 		if( collections[i].indexOf("blog_collection") == 0){
@@ -11,23 +11,23 @@ function covert_collections_to_json(collections_str)
 			 break;
 		}
 	}
-	console.log(blog_collection);
+	//console.log(blog_collection);
 	if(blog_collection == null) return null;
 	
 	var blogs = [];
 	blog_collection = blog_collection.match(/[^\r\n]+/g);
 	for(i in blog_collection){
-		console.log(blog_collection[i]);
+		//console.log(blog_collection[i]);
 		var  x = blog_collection[i].match(/file:(.*).md/i)
 		if(x){
 			var name = x[1].split("/").pop();
-			console.log(name);
+			//console.log(name);
 			var blog = {"name" : name, "href" : x[1]+".md"};
 			blogs.push(blog);
 		}
 	}
 
-	console.log(blogs);
+	//console.log(blogs);
     return blogs;	
 } 
 
