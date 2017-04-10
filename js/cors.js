d508c3f937d4a3d2c5a596ba067f623382991229
Truman Zhou <Truman.Zhou@emc.com>
2017-04-11 05:09:12 +0800

    add bootstrap

---
+++ js/cors.js
@@ -0,0 +1,44 @@
+
+// Create the XHR object.
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+
+// Helper method to parse the title tag from the response.
+function getTitle(text) {
+  return text.match('<title>(.*)?</title>')[1];
+}
+
+// Make the actual CORS request.
+function makeCorsRequest(url, onload, onerror) {
+  
+  var xhr = createCORSRequest('GET', url);
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
+
+  // Response handlers.
+  xhr.onload = function() {
+	onload(xhr.responseText);
+//    var text = xhr.responseText;
+//    var title = getTitle(text);
+//    alert('Response from CORS request to ' + url + ': ' + title);
+  };
+
+  xhr.onerror = onerror;
+
+  xhr.send();
+}
